5c3146116808fd221fd2f9e0fa1dec818de8134b
Fraser Scott <admin@thunr.com>
2014-01-16 14:38:21 +0000

    disable default site

--- attributes/nginx.rb
+++ attributes/nginx.rb
@@ -1,3 +1,4 @@
 override['nginx']['install_method'] = 'package'
 override['nginx']['version'] = '1.4.3'
 override['nginx']['repo_source'] = 'nginx'
+override['nginx']['default_site_enabled'] = false
